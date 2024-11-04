73337225cf787405dd77b0a2bc8d37779e479a5f
RahmanKM <parizat2@gmail.com>
2024-11-04 16:04:21 +0100

    Updated custom xpath

--- src/main/java/seleniumPractice/Custom_Xpath.java
+++ src/main/java/seleniumPractice/Custom_Xpath.java
@@ -14,10 +14,10 @@ public class Custom_Xpath {
 		//1. abs xpath: tag by tag -> we don't use
 		// 2. relative/custom xpath:
 		
-		//htmltag[@attribute='']
-		
-		//text():
-		//htmltag[text()='']
+//		//htmltag[@attribute='']
+//		
+//		//text():
+//		//htmltag[text()='']
 		
 	
 }
